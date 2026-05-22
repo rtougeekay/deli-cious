2a4fb1b10429f2e723a8ceb40fa97513e66b8650
J Richard K Tougeekay <rtougeekay@gmail.com>
2026-05-22 13:38:52 -0500

    drink prices and sizes. if and else statements.

--- src/main/java/com/pluralsight/Drink.java
+++ src/main/java/com/pluralsight/Drink.java
@@ -1,4 +1,25 @@
 package com.pluralsight;
 
 public class Drink {
+    private String size;
+    private String flavor;
+
+    public Drink(String size, String flavor) {
+        this.size = size;
+        this.flavor = flavor;
+    }
+
+    public double getPrice() {
+        if (size.equalsIgnoreCase("small")) {
+            return 2.00;
+        } else if (size.equalsIgnoreCase("medium")) {
+            return 2.50;
+        } else {
+            return 3.00;
+        }
+    }
+
+    public String toString() {
+        return size + " " + flavor + " drink - $" + getPrice();
+    }
 }
